11e8e5adda6e9045eff6470e71ffa07e5576149e
Josh Martin <joshmartin0212@gmail.com>
2024-10-02 22:36:43 -0700

    chore: cleaned up login and user logic

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,29 @@ import Resume from './components/Resume';
 import Dashboard from './components/Dashboard';
 import Settings from './components/Settings';
 import Research from './components/Research';
-import { UserProvider } from './context/UserContext';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <UserProvider>
-        <Router>
-          <Header />
-          <div style={{ display: 'flex' }}>
-            <div style={{ flex: 1 }}>
-              <main className="flex-grow overflow-auto" style={{ maxHeight: 'calc(100vh - 64px - 64px)' }}>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/dashboard" element={<Dashboard />} />
-                  <Route path="/settings" element={<Settings />} />
-                  <Route path="/research" element={<Research />} />
-                  <Route path="/resume" element={<Resume />} />
-                </Routes>
-              </main>
-            </div>
+      <Router>
+        <Header />
+        <div style={{ display: 'flex' }}>
+          <div style={{ flex: 1 }}>
+            <main className="flex-grow overflow-auto" style={{ maxHeight: 'calc(100vh - 64px - 64px)' }}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/research" element={<Research />} />
+                <Route path="/resume" element={<Resume />} />
+              </Routes>
+            </main>
           </div>
-        </Router>
-        <Footer />
-      </UserProvider>
+        </div>
+      </Router>
+      <Footer />
     </Provider>
   );
 }
